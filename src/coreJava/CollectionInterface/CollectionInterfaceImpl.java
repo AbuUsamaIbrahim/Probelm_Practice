d77c124583bce06bace73772c517f817661a8643
Mahadi024 <mahadihasan.parash@gmail.com>
2022-02-14 17:31:02 +0600

    Testing git 1

--- src/coreJava/CollectionInterface/CollectionInterfaceImpl.java
+++ src/coreJava/CollectionInterface/CollectionInterfaceImpl.java
@@ -5,8 +5,10 @@ import java.util.*;
 public class CollectionInterfaceImpl {
  public static void main(String[] args){
      List<String>stringList = new ArrayList<>();
+     List<String>stringList1 = new LinkedList<>();
      Set<String> stringSet = new HashSet<>();
      Set<String> stringSet1 = new TreeSet<>();
+     Set<String> stringSet2 = new LinkedHashSet<>();
      Map<Long,String>longStringMap = new HashMap<>();
      Map<Long,String>longStringMap1 = new TreeMap<>();
      Map<Long,String>longStringMap2 = new Hashtable<>();
